b424da18aa51e7edac39a1665b99a976f13b4420
antoinehermant <antoine.hermant@unibe.ch>
2025-08-29 17:25:07 +0200

    feat|Compile: Takes file type
    Some IRH raw files are divided either by IRH layers or by radar traces.
    Now Compile class can handle both structures. It also takes wave speed
    and firn correction when the data was not already converted from TWT.

--- compiling/compile.py
+++ compiling/compile.py
@@ -4,10 +4,14 @@ import pandas as pd
 import numpy as np
 import glob
 from pyproj import Transformer
+from typing import Union
 
 class CompileDatabase:
-    def __init__(self, dir_list: list[str]) -> None:
+    def __init__(self, dir_list: list[str], file_type: str = 'layer', wave_speed: Union[None, float] = None, firn_correction: Union[None, float] = None) -> None:
         self.dir_list = dir_list
+        self.wave_speed = wave_speed
+        self.firn_correction = firn_correction
+        self.file_type = file_type
 
     def get_dict_ages(self, tab_file) -> dict:
         ages = pd.read_csv(tab_file, header=None, sep='\t', names=['file', 'age'])
@@ -38,17 +42,8 @@ class CompileDatabase:
                 _, file_name = os.path.split(file)
                 file_name_, ext = os.path.splitext(file_name)
 
-                ds = ds[original_new_columns.columns.values]  # Selecting columns of interest
-                ds.columns = original_new_columns.iloc[0].values  # renaming the columns
-
-                age = ages[file_name_]
-                ds = ds.rename(columns={'IRHdepth': age})
-
-                ds['Trace_ID'] = ds['Trace_ID'].astype(str)
-                ds['Trace_ID'] = ds['Trace_ID'].str.replace(r'/\s+', '_') # Replace slashes with underscores, otherwise the paths can get messy
-                ds['Trace_ID'] = ds['Trace_ID'].str.replace('/', '_')
-
-                ds.set_index('Trace_ID', inplace=True)
+                ds = ds[ds.columns.intersection(original_new_columns.columns)]
+                ds.columns = original_new_columns[ds.columns].iloc[0].values  # renaming the columns
 
                 if 'x' not in ds.columns and 'y' not in ds.columns:
                     if 'lon' in ds.columns and 'lat' in ds.columns:
@@ -72,10 +67,47 @@ class CompileDatabase:
                     print('No coordinates found in the dataset, exiting ....')
                     sys.exit()
 
-                for trace_id in np.unique(ds.index):
-                    ds_trace = ds.loc[trace_id]
-                    ds_trace_file = f'{dir_path}/pkl/{trace_id}/{file_name_}.pkl'
+                if self.file_type == 'layer':
+                    age = ages[file_name_]
+                    ds = ds.rename(columns={'IRHdepth': age})
 
-                    os.makedirs(f'{dir_path}/pkl/{trace_id}' , exist_ok=True)
-                    ds_trace.to_pickle(ds_trace_file)
-                    print(ds_trace_file)
+                    ds['Trace_ID'] = ds['Trace_ID'].astype(str)
+                    ds['Trace_ID'] = ds['Trace_ID'].str.replace(r'/\s+', '_') # Replace slashes with underscores, otherwise the paths can get messy
+                    ds['Trace_ID'] = ds['Trace_ID'].str.replace('/', '_')
+
+                    ds.set_index('Trace_ID', inplace=True)
+
+                    for trace_id in np.unique(ds.index):
+                        ds_trace = ds.loc[trace_id]
+                        ds_trace_file = f'{dir_path}/pkl/{trace_id}/{file_name_}.pkl'
+
+                        os.makedirs(f'{dir_path}/pkl/{trace_id}' , exist_ok=True)
+                        ds_trace.to_pickle(ds_trace_file)
+                        print(ds_trace_file)
+
+                elif self.file_type == 'trace':
+                    trace_id = file_name_
+                    ages = {key: ages[key] for key in ds.columns if key in ages}
+
+                    for IRH in ages:
+                        age_impl = ages.get(IRH)
+                        ds = ds.rename(columns={IRH: age_impl})
+
+                    for IRH in ages:
+                        ds_IRH = ds[ages.get(IRH)]
+                        if self.wave_speed:
+                            ds_IRH *= self.wave_speed
+                        if self.firn_correction:
+                            ds_IRH += self.firn_correction
+                        ds_IRH = pd.DataFrame({
+                            'x': ds['x'],
+                            'y': ds['y'],
+                            'lon': ds['lon'],
+                            'lat': ds['lat'],
+                            ages.get(IRH): ds_IRH,
+                        })
+                        ds_trace_file = f'{dir_path}/pkl/{trace_id}/{IRH}.pkl'
+
+                        os.makedirs(f'{dir_path}/pkl/{trace_id}' , exist_ok=True)
+                        ds_IRH.to_pickle(ds_trace_file)
+                        print(ds_trace_file)
