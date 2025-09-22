d8bc8211f60d7003c7511dc82abbc53ea56e3a66
antoinehermant <antoine.hermant@unibe.ch>
2025-09-22 20:56:07 +0200

    docs: Add MIT LICENSE for distribution

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     author_email= "antoine.hermant@etik.com",
     url="https://github.com/antoinehermant/anta_database",
     description= "SQLite database for the AntArchitecture Community Data",
+    license="MIT",
     long_description="""""",
     packages=find_packages(),
     package_data={
